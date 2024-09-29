5a4a8da3344cb2f735b0af602a2243e5051bf381
Brian Kelley <bhk@bhk.com>
2024-09-28 23:00:36 -0700

    Package cleanup

--- smark/smark.mk
+++ smark/smark.mk
@@ -5,13 +5,12 @@
 SmarkDoc.inherit    = _SmarkDoc
 _SmarkDoc.inherit    = Exec
 _SmarkDoc.outExt     = .html
-_SmarkDoc.command    = {exportPrefix} {exe} -o {@} {flags} -- {^}
+_SmarkDoc.command    = {exportPrefix} {smarkExe} -o {@} {flags} -- {^}
 _SmarkDoc.css        =
 _SmarkDoc.depsFile   = {@}.dep
 _SmarkDoc.flags      = $(patsubst %,--no-default-css --css='%',{css}) {warnFlags} --deps={depsFile}
 _SmarkDoc.warnFlags  = --error
-_SmarkDoc.exe        = {smarkDir}smark
-_SmarkDoc.up         = {exe}
+_SmarkDoc.smarkExe  := $(dir $(lastword $(MAKEFILE_LIST)))smark
+_SmarkDoc.up         = {smarkExe}
 _SmarkDoc.exports    = SMARK_PATH
 _SmarkDoc.SMARK_PATH = ./?.lua
-_SmarkDoc.smarkDir  := $(dir $(lastword $(MAKEFILE_LIST)))
